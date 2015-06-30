a6f714707c070f1bc9ae3325e053d1213426beb0
Markus Cisler <hello@markuscisler.com>
2015-06-30 20:33:40 +0200

    it works

--- app.py
+++ app.py
@@ -11,23 +11,23 @@ config.read('config.ini')
 
 @app.route('/', methods=['POST', 'GET'])
 def hi():
+    client_id = config.get('foursquare', 'client_id')
+    client_secret = config.get('foursquare', 'client_secret')
+    redirect_uri = config.get('foursquare', 'redirect_uri')
+    client = foursquare.Foursquare(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+
     if request.method == 'POST':
         data = json.loads(request.data)
         if data.get(u'device') == config.get('geofency', 'device_name'):
-            checkin(data.get(u'name'))
-            return "OK"
+            access_code = config.get('foursquare', 'access_code')
+            client.set_access_token(client.oauth.get_token(access_code))
 
+            location = data.get(u'name')
+            if config.get('venues', location):
+                client.checkins.add(params={'venueId': config.get('venues', location)})
+                return "OK"
     else:
-        client_id = config.get('foursquare', 'client_id')
-        client_secret = config.get('foursquare', 'client_secret')
-        redirect_uri = config.get('foursquare', 'redirect_uri')
-
-        client = foursquare.Foursquare(client_id=client_id,
-                                       client_secret=client_secret,
-                                       redirect_uri=redirect_uri)
-
-        auth_uri = client.oauth.auth_url()
-        print "Open: %s" % auth_uri
+        return "<a href=\"%s\"><h1>DO THE OAUTH</h1></a>" % client.oauth.auth_url()
 
 
 @app.route('/oauth2/authorize', methods=['GET'])
@@ -36,24 +36,13 @@ def oauth_authorize():
     config.set('foursquare', 'access_code', code)
     with open('config.ini', 'wb') as cfg:
         config.write(cfg)
-    return code
+    return "done, set up your webhook now"
 
 
-def checkin(loc):
-    client_id = config.get('foursquare', 'client_id')
-    client_secret = config.get('foursquare', 'client_secret')
-    redirect_uri = config.get('foursquare', 'redirect_uri')
-    access_code = config.get('foursquare', 'access_code')
-
-    client = foursquare.Foursquare(client_id=client_id,
-                                   client_secret=client_secret,
-                                   redirect_uri=redirect_uri)
-    access_token = client.oauth.get_token(access_code)
-    client.set_access_token(access_token)
-
-    # if loc == "test":
-    #     client.checkins.add(params={'venueId': '5556ff71498ede07439e5d24'})
-
+@app.route("/debug", methods=['POST'])
+def test():
+    data = json.loads(request.data)
+    print data
     return "OK"
 
 
