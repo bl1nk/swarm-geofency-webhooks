01fd61a5a37142741b7b036fc81245ac2e440ea4
Markus Cisler <hello@markuscisler.com>
2015-07-01 00:33:32 +0200

    🐝

--- app.py
+++ app.py
@@ -10,25 +10,27 @@ config.read('config.ini')
 
 @app.route('/', methods=['POST', 'GET'])
 def hi():
-    client_id = config.get('foursquare', 'client_id')
-    client_secret = config.get('foursquare', 'client_secret')
-    redirect_uri = config.get('foursquare', 'redirect_uri')
-    client = foursquare.Foursquare(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+    client = foursquare.Foursquare(
+        client_id=config.get('foursquare', 'client_id'),
+        client_secret=config.get('foursquare', 'client_secret'),
+        redirect_uri=config.get('foursquare', 'redirect_uri'))
 
     if request.method == 'POST':
         device = request.form.get('device')
         location = request.form.get('name')
 
         if config.has_section(device):
-            client.set_access_token(client.oauth.get_token(config.get('foursquare', 'access_code')))
+            client.set_access_token(
+                client.oauth.get_token(config.get('foursquare', 'access_code')))
             if config.has_option(device, location):
-                client.checkins.add(params={'venueId': config.get(device, location)})
+                client.checkins.add(
+                    params={'venueId': config.get(device, location)})
                 return "OK"
 
     else:
         if config.has_option('foursquare', 'access_code'):
-            return "nope"
-        return "<a href=\"%s\"><h1>DO THE OAUTH</h1></a>" % client.oauth.auth_url()
+            return ""
+        return "<a href=\"%s\">click me</a>" % client.oauth.auth_url()
 
 
 @app.route('/oauth2/authorize', methods=['GET'])
